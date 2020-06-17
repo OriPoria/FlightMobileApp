98ce19edc76a74173ecee6d5ea35c0bd18d1c2fe
shmuel suna <shmuel7778@gmail.com>
2020-06-17 19:13:14 +0300

    adding rudder/throttle

--- app/src/main/java/com/example/flightmobileapp/SimulatorActivity.kt
+++ app/src/main/java/com/example/flightmobileapp/SimulatorActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import android.widget.SeekBar
+import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.lifecycle.Observer
 import com.example.flightmobileapp.network.SimulatorProperty
@@ -17,11 +19,53 @@ import okhttp3.ResponseBody
 
 class SimulatorActivity : AppCompatActivity() {
     val vm : OverviewViewModel by viewModels()
+    lateinit var ThrottleSeekbar :SeekBar
+    lateinit var valueOfThrottleSeekBar :TextView
+    lateinit var RudderSeekbar :SeekBar
+    lateinit var valueOfRudderSeekBar :TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_simulator)
         setObserver()
+
+        ThrottleSeekbar = findViewById(R.id.ThrottleseekBar) as SeekBar
+        valueOfThrottleSeekBar = findViewById(R.id.ThrottleValuetextView) as TextView
+        RudderSeekbar = findViewById(R.id.RudderseekBar) as SeekBar
+        valueOfRudderSeekBar = findViewById(R.id.RudderValueTextView) as TextView
+
+        RudderSeekbar.max =100
+        RudderSeekbar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, progress: Int, p2: Boolean) {
+                valueOfRudderSeekBar.text = progress.toString()
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            }
+
+        })
+
+        ThrottleSeekbar.max =100
+        ThrottleSeekbar.setSaveEnabled(false)
+        ThrottleSeekbar.setOnSeekBarChangeListener(object  : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, progress: Int, p2: Boolean) {
+                valueOfThrottleSeekBar.text = progress.toString()
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+            }
+
+        })
+
+
 
         val handler = Handler()
         //add this task to the handler loop every 2 seconds to update the view
@@ -43,6 +87,7 @@ class SimulatorActivity : AppCompatActivity() {
             simImg.setImageBitmap(B)
         })
     }
+
 
 
 }
