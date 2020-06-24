2feba5340500b6f71aae9f13437fe5a659b132f1
ori poria <ori.poria@live.biu.ac.il>
2020-06-24 10:59:30 +0300

    change post to callback

--- app/src/main/java/com/example/flightmobileapp/overview/SimulatorViewModel.kt
+++ app/src/main/java/com/example/flightmobileapp/overview/SimulatorViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.flightmobileapp.overview
 
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -11,11 +12,14 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import retrofit2.Call
 import okhttp3.ResponseBody
+import retrofit2.Callback
+import retrofit2.Response
 import java.net.SocketTimeoutException
 
 
-class SimulatorViewModel {
+class SimulatorViewModel constructor(s: SimulatorApiService) {
 
     // The internal MutableLiveData Img that stores the most recent response
     private val _response = MutableLiveData<ResponseBody>()
@@ -31,14 +35,11 @@ class SimulatorViewModel {
     private var viewModelJob = Job()
 
     //var simulatorApiService:MutableLiveData<SimulatorApiService> = MutableLiveData()
-    var myService: SimulatorApiService
+    var myService: SimulatorApiService = s
 
     //Create a coroutine scope for that new job using the main dispatcher
     val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.Main )
 
-    constructor(s: SimulatorApiService) {
-        myService = s
-    }
     fun getSimulatorImg() {
         coroutineScope.launch {
         //Calling getSimulator() from the SimulatorApi service creates and starts the network call on a background thread,
@@ -49,10 +50,13 @@ class SimulatorViewModel {
             _response.value = (imgResult)
             _isErr.value = false
 
+
+            //Error from server handling
         } catch (e: Exception) {
-            if (e.localizedMessage.toString().startsWith("timeout")) {
-                errMsg = "Connecion with the server problem: Timeout of get image"
-            } else {errMsg="Connecion with the server problem. Try to re-connect from the main menu"}
+            val strErr:String? = e.localizedMessage
+            if (strErr != null && strErr.startsWith("timeout")) {
+                errMsg = "Connection with the server problem: Timeout of get image"
+            } else {errMsg="Connection with the server problem. Try to re-connect from the main menu"}
             _isErr.value = true
         }
 
@@ -63,18 +67,19 @@ class SimulatorViewModel {
     //send a post request to the server
     fun sendCmd(simInfo:SimulatorProperty){
         coroutineScope.launch {
-            try {
-                val msgReturned = myService.sendCommand(simInfo).await()
-                Log.i("msg", "post succeed")
-                _isErr.value = false
+                myService.sendCommand(simInfo).enqueue(object : Callback<ResponseBody> {
+                    override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                        errMsg = "Connection with the server problem: failure sending values"
+                        _isErr.value = true
+                    }
+                    override fun onResponse(
+                        call: Call<ResponseBody>,
+                        response: Response<ResponseBody>
+                    ) {
+                        _isErr.value = false
+                    }
+                })
 
-            //In case there was a bad request, and the post did'nt succeed
-            } catch (e: Exception) {
-                if (e.message.toString().startsWith("timeout")) {
-                    errMsg = "Connecion with the server problem: Timeout of send values"
-                } else {errMsg="Connecion with the server problem. Try to re-connect from the main menu"}
-                _isErr.value = true
-            }
 
         }
 
